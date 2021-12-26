bb4135a0dcbea0f1ffabbe694e843c070ec496ba
Luiz L Barreto <luiz.l.barreto@exxonmobil.com>
2021-12-25 23:45:55 -0300

    Fix netlify build

--- src/components/UnitedFooter.vue
+++ src/components/UnitedFooter.vue
@@ -4,9 +4,9 @@
         class="h-24 px-12 bg-gradient-to-l from-brandPrimary to-brandSecondary flex justify-end items-center"
     >
         <img class="text-white p-4" src="../assets/MdiLightbulbOn.svg" />
-        <img class="fill-white p-4" src="../assets/ionvolumehigh.svg" />
+        <img class="fill-white p-4" src="../assets/IonVolumeHigh.svg" />
         <img class="fill-white p-4" src="../assets/united_service.svg" />
-        <img class="fill-white p-4" src="../assets/mdipower.svg" />
+        <img class="fill-white p-4" src="../assets/MdiPower.svg" />
     </footer>
 </template>
 
